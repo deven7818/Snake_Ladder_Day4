2375b75ef498267eb2d77589df6fa14dc2646534
deven7818 <deven7818@gmail.com>
2022-02-16 13:07:17 +0530

    Snake Ladder postion to 0 uc1

---
+++ Snake_Ladder/src/main/java/com/snake_ladder/Snake_Ladder.java
@@ -0,0 +1,12 @@
+package com.snake_ladder;
+
+public class Snake_Ladder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Lader Simulator");
+		int playerPosition = 0;
+		System.out.println("Starting Position is " +playerPosition);
+
+	}
+
+}
