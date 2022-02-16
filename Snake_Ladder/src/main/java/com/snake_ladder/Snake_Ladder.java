e1b9e41e4fdc44369ccca4b6dc251598211bea25
deven7818 <deven7818@gmail.com>
2022-02-16 16:24:27 +0530

    Random No Play,Snake,Ladder uc3

--- Snake_Ladder/src/main/java/com/snake_ladder/Snake_Ladder.java
+++ Snake_Ladder/src/main/java/com/snake_ladder/Snake_Ladder.java
@@ -1,5 +1,7 @@
 package com.snake_ladder;
 
+import java.util.Scanner;
+
 public class Snake_Ladder {
 
 	public static void main(String[] args) {
@@ -7,6 +9,33 @@ public class Snake_Ladder {
 		int playerPosition = 0;
 		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 		System.out.println("Dice role : " + die );
+		
+		System.out.println("Enter Player Name : ");
+		Scanner sc = new Scanner(System.in);
+		String playerName = sc.next();
+		
+		System.out.println("Player Name : " +playerName);
+		
+		int option = (int) (Math.floor(Math.random() * 10) % 3 );
+		
+		switch (option) {
+		case 0 :
+			System.out.println("No play");
+			playerPosition += 0;
+			break;
+			
+		case 1 :
+			System.out.println("Ladder");
+			playerPosition += die;
+			break;
+			
+		default :
+			System.out.println("Snake");
+			playerPosition -= die;
+			if(playerPosition < 0)
+				playerPosition = 0;		
+		}
+		System.out.println("Player position : " + playerPosition);
 
 	}
 
