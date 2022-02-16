facd0ee414b13f97589ebb3fb936504cf8b61089
deven7818 <deven7818@gmail.com>
2022-02-16 20:40:44 +0530

    exact Winning Position 100 uc5

--- Snake_Ladder/src/main/java/com/snake_ladder/Snake_Ladder.java
+++ Snake_Ladder/src/main/java/com/snake_ladder/Snake_Ladder.java
@@ -19,28 +19,35 @@ public class Snake_Ladder {
 
 		while (winningPosition > playerPosition) {
 
+			// get random number for dice between 1-6
 			int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 			System.out.println("Dice role : " + die);
 
+			// get random number for snake , ladder ,no play
 			int option = (int) (Math.floor(Math.random() * 10) % 3);
 
-			switch (option) {
-			case 0:
-				System.out.println("No play :" + 0);
-				playerPosition += 0;
-				break;
+			int remainigPosition = winningPosition - playerPosition;
 
-			case 1:
-				System.out.println("Ladder : " + "+" + die);
-				playerPosition += die;
-				break;
+			if (remainigPosition >= die) {
 
-			default:
-				System.out.println("Snake : " + "+" + die);
-				playerPosition -= die;
-				if (playerPosition < 0)
-					playerPosition = 0;
-			}
+				switch (option) {
+				case 0:
+					System.out.println("No play :" + 0);
+					playerPosition += 0;
+					break;
+
+				case 1:
+					System.out.println("Ladder : " + "+" + die);
+					playerPosition += die;
+					break;
+
+				default:
+					System.out.println("Snake : " + "+" + die);
+					playerPosition -= die;
+					if (playerPosition < 0)
+						playerPosition = 0;
+				}// end of switch
+			} // end of if
 		}
 		System.out.println("Player position : " + playerPosition);
 
